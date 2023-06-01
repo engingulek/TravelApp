04d488a8f03e8b47c589a4b00191d91f85dd7c4e
Engin Gülek <engingulek0@gmail.com>
2023-06-01 14:09:29 +0300

    Create App

---
+++ TravelApp/TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by engin gülek on 1.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TravelApp/TravelAppUITests/TravelAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TravelAppUITestsLaunchTests.swift
+//  TravelAppUITests
+//
+//  Created by engin gülek on 1.06.2023.
+//
+
+import XCTest
+
+final class TravelAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
